57b6ebf6551ccb7687072ea47a79f6350096de24
Surya V <76275102+SuryaVelumani@users.noreply.github.com>
2021-12-22 21:11:49 +0530

    Update TestAddingPage.js

--- src/admin_screens/TestAddingPage.js
+++ src/admin_screens/TestAddingPage.js
@@ -11,16 +11,21 @@ const TestAddingPage = () => {
     const [questions, setquestions] = useState([]);
     const [questionTitle, setQuestionTitle] = useState("");
     const [questionOptions, setQuestionOptions] = useState("");
-    const [correctOption,setCorrectOption] = useState("");
+    const [correctOption, setCorrectOption] = useState("");
     const { subject } = useParams();
     let navigate = useNavigate();
 
-    const questionAdder = () =>{
-        if(qCount > 0){
+
+    let errorsObj = { questionTitle: '', questionOptions: '', correctOption: '' };
+    const [errors, setErrors] = useState(errorsObj);
+
+
+    const questionAdder = () => {
+        if (qCount > 0) {
             let questionObj = {
                 question: questionTitle,
-                options:questionOptions.split(","),
-                correctOption:correctOption
+                options: questionOptions.split(","),
+                correctOption: correctOption
             }
             setQuestionTitle("");
             setQuestionOptions("");
@@ -29,88 +34,141 @@ const TestAddingPage = () => {
             temp.push(questionObj);
             setquestions(temp);
             console.log(questions);
-            setQCount(qCount-1);
+            setQCount(qCount - 1);
         }
     }
 
-    const testAdder = ()=>{
-        axios.post("https://exampal.herokuapp.com/admin/createTest",{
-            subjectName:subject,
-            testName:testName,
-            testQuestions:questions
-        },{
-            headers:{
+    function onPost(e) {
+        e.preventDefault();
+        let error = false;
+        const errorObj = { ...errorsObj };
+        if (questionTitle == "") {
+            errorObj.questionTitle = 'Please Add the question';
+            console.log('Add question');
+            error = true;
+        }
+        if (questionOptions.length < 8) {
+            errorObj.questionOptions = 'Please Add 4 options with commas in between';
+            error = true;
+        }
+        if (correctOption == "") {
+            errorObj.correctOption = 'Please Add the correct answer';
+            error = true;
+        }
+        setErrors(errorObj);
+    }
+
+    function onAddTest(e) {
+        //const qCountRegex = "[0-9]*"
+        e.preventDefault();
+        let error = false;
+        const errorObj = { ...errorsObj };
+        if (testName == "") {
+            errorObj.testName = 'Please Add the test name';
+            console.log('Add subject');
+            error = true;
+        }
+        if (qCount == "") {
+            errorObj.qCount = 'Please Add the number of questions';
+            error = true;
+        }
+        setErrors(errorObj);
+    }
+    const testAdder = () => {
+        axios.post("https://exampal.herokuapp.com/admin/createTest", {
+            subjectName: subject,
+            testName: testName,
+            testQuestions: questions
+        }, {
+            headers: {
                 Authorization: `Bearer ${sessionStorage.getItem("userToken")}`
             }
         })
-        .then(()=>{
-            navigate(`/admin/${subject}`)
-        })
-        .catch((err)=>{
-            alert(`Error at test uploading: ${err}`)
-        })
+            .then(() => {
+                navigate(`/admin/${subject}`)
+            })
+            .catch((err) => {
+                alert(`Error at test uploading: ${err}`)
+            })
     }
 
     return (
         <div className='h-screen bg-warning'>
-            <NavBar/>
+            <NavBar />
             <div className='h-3/4 flex justify-center items-center'>
-                <div className={`${promptState} flex flex-col justify-center bg-primary p-10 rounded-2xl shadow-lg space-y-5`}>
-                    <label htmlFor="">Test Name</label>
-                    <input 
-                    value={testName}
-                    onChange={(e)=>{setTestName(e.target.value)}}
-                    className="p-3 rounded-2xl" type="text" />
-                    <label htmlFor="">Number of questions</label>
-                    <input 
-                    onChange={(e)=>setQCount(e.target.value)}
-                    className="p-3 rounded-2xl" type="text" />
-                    <button 
-                    onClick={()=>{
-                        if(qState === "hidden"){
-                            setQState("block")
-                            setPromptState("hidden")
-                        }
-                    }}  
-                    className='bg-tertiary p-5 rounded-2xl text-primary'>
-                        Add Questions
-                    </button>
-                </div> 
-                <div className={`${qState} bg-primary p-10 rounded-2xl flex flex-col space-y-5`}>
-                    <label htmlFor="">Question</label>
-                    <input 
-                    value={questionTitle}
-                    onChange={(e)=>{setQuestionTitle(e.target.value)}}
-                    className="p-3 rounded-2xl" type="text" />
-                    <label htmlFor="">Options</label>
-                    <input 
-                    value={questionOptions}
-                    onChange={(e)=>{setQuestionOptions(e.target.value)}}
-                    className="p-3 rounded-2xl" type="text" />
-                    <label htmlFor="">Correct Option</label>
-                    <input 
-                    value={correctOption}
-                    onChange={(e)=>{setCorrectOption(e.target.value)}}
-                    className="p-3 rounded-2xl"
-                    type="text" />
-                    {qCount === 0 ? (
+                <form onSubmit={onAddTest}>
+                    <div className={`${promptState} flex flex-col justify-center bg-primary p-10 rounded-2xl shadow-lg space-y-5`}>
+                        <label htmlFor="">Test Name</label>
+                        <input
+                            value={testName}
+                            onChange={(e) => { setTestName(e.target.value) }}
+                            className="p-3 rounded-2xl" type="text" />
+                        {errors.testName && <div className='text-red-600'>{errors.testName}</div>}
+
+                        <label htmlFor="">Number of questions</label>
+                        <input
+                            onChange={(e) => setQCount(e.target.value)}
+                            className="p-3 rounded-2xl" type="text" />
+                        {errors.qCount && <div className='text-red-600'>{errors.qCount}</div>}
+
                         <button
-                        onClick={testAdder}
-                        className='bg-tertiary rounded-xl p-3 text-primary'>
-                            Post Test
+                            type="submit"
+                            onClick={() => {
+                                if (qState === "hidden") {
+                                    setQState("block")
+                                    setPromptState("hidden")
+                                }
+                            }}
+                            className='bg-tertiary p-5 rounded-2xl text-primary'>
+                            Add Questions
                         </button>
-                    ):(
-                        <button 
-                        onClick={questionAdder}
-                        className='bg-tertiary rounded-xl p-3 text-primary'>
-                            + Add 
-                        </button>
-                    )}
-                    
-                </div>
+                    </div>
+                </form>
+                <form onSubmit={onPost}>
+                    <div className={`${qState} bg-primary p-10 rounded-2xl flex flex-col space-y-5`}>
+                        <label htmlFor="">Question</label>
+                        <input
+                            value={questionTitle}
+                            onChange={(e) => { setQuestionTitle(e.target.value) }}
+                            className="p-3 rounded-2xl" type="text" />
+                        {errors.questionTitle && <div className='text-red-600'>{errors.questionTitle}</div>}
+
+                        <label htmlFor="">Options</label>
+                        <input
+                            value={questionOptions}
+                            onChange={(e) => { setQuestionOptions(e.target.value) }}
+                            className="p-3 rounded-2xl" type="text" />
+                        {errors.questionOptions && <div className='text-red-600'>{errors.questionOptions}</div>}
+
+                        <label htmlFor="">Correct Option</label>
+                        <input
+                            value={correctOption}
+                            onChange={(e) => { setCorrectOption(e.target.value) }}
+                            className="p-3 rounded-2xl"
+                            type="text" />
+                        {errors.correctOption && <div className='text-red-600'>{errors.correctOption}</div>}
+
+                        {qCount === 0 ? (
+                            <button
+                                onClick={testAdder}
+                                className='bg-tertiary rounded-xl p-3 text-primary'>
+                                Post Test
+                            </button>
+                        ) : (
+                            <button
+                                type="submit"
+                                onClick={questionAdder}
+                                className='bg-tertiary rounded-xl p-3 text-primary'>
+                                + Add
+                            </button>
+                        )}
+
+                    </div>
+                </form>
             </div>
         </div>
     )
 }
 
 export default TestAddingPage
+
